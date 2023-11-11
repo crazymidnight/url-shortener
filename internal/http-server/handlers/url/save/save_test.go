7d2f1abd7d769e78de61fa57e15a1495007019e1
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2023-11-11 17:14:05 +0700

    add handler tests

---
+++ internal/http-server/handlers/url/save/save_test.go
@@ -0,0 +1,93 @@
+package save_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"url-shortener/internal/http-server/handlers/url/save"
+	"url-shortener/internal/http-server/handlers/url/save/mocks"
+	"url-shortener/internal/lib/logger/handlers/slogdiscard"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveHandler(t *testing.T) {
+	cases := []struct {
+		name      string
+		alias     string
+		url       string
+		respError string
+		mockError error
+	}{
+		{
+			name:  "Success",
+			alias: "test_alias",
+			url:   "https://google.com",
+		},
+		{
+			name:  "Empty alias",
+			alias: "",
+			url:   "https://google.com",
+		},
+		{
+			name:      "Empty URL",
+			alias:     "some_alias",
+			url:       "",
+			respError: "Field URL is required",
+		},
+		{
+			name:      "Invalid URL",
+			alias:     "some_alias",
+			url:       "some invalid url",
+			respError: "Field URL is not valid URL",
+		},
+		{
+			name:      "SaveURL error",
+			alias:     "test_alias",
+			url:       "https://google.com",
+			respError: "Error while URL saving",
+			mockError: errors.New("Unexpected error"),
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			urlSaverMock := mocks.NewURLSaver(t)
+
+			if tc.respError == "" || tc.mockError != nil {
+				urlSaverMock.On("SaveURL", tc.url, mock.AnythingOfType("string")).
+					Return(int64(1), tc.mockError).
+					Once()
+			}
+
+			handler := save.New(slogdiscard.NewDiscardLogger(), urlSaverMock)
+
+			input := fmt.Sprintf(`{"url": "%s", "alias": "%s"}`, tc.url, tc.alias)
+
+			req, err := http.NewRequest(http.MethodPost, "/url", bytes.NewReader([]byte(input)))
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
+
+			require.Equal(t, rr.Code, http.StatusOK)
+
+			body := rr.Body.String()
+
+			var resp save.Response
+
+			require.NoError(t, json.Unmarshal([]byte(body), &resp))
+
+			require.Equal(t, tc.respError, resp.Error)
+		})
+	}
+}
